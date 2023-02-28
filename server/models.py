98baae99e8e461c7acb1d21d9e196ba9e7e7c43a
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-02-27 23:06:50 -0500

    passed tests

--- server/models.py
+++ server/models.py
@@ -6,13 +6,35 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', backref = 'zookeeper')
+
+    def __repr__(self):
+        return f'<Zookeeper {self.name}>'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', backref = 'enclosure')
+
+    def __repr__(self):
+        return f'<Enclosure {self.id}, {self.environment}>'
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+
+    def __repr__(self):
+        return f'<Animal {self.name}, {self.species}>'
